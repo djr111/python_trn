e40a641b8d37079f9756f6e47d00d44a367a8b25
djr111 <djr111@inbox.lv>
2023-12-05 23:53:37 +0000

    updated file

--- hi.py
+++ hi.py
@@ -1,1 +1,20 @@
 print("hello world")
+
+name = input("whats your name ? ")
+print(f"Nice to meet you {name}")
+
+number1 = input("Enter first number please: ")
+number2 = input("Enter second number please: ")
+
+sum = int(number1) + int(number2)
+difference = int(number1) - int(number2)
+product = int(number1) * int(number2)
+quotient = int(number1) / int(number2)
+
+print(f"Summ of two numbers : {sum}")
+print(f"         difference : {difference}")
+print(f"             product: {product}")
+print(f"            quotient: {quotient}")
+
+age = input("what is you age ? ")
+print(f"Oh next year you will be{int(age)+1} years old")
